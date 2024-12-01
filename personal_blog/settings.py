ef7bf37b8e1f2eb46a57b3bf0216c1b71488c03b
gab-i-alves <gab.alvesnacimentO@gmail.com>
2024-12-01 20:26:33 -0300

    feat: Add url consts on settings

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -100,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_URL = 'blog:login'
+LOGIN_REDIRECT_URL = 'blog:dashboard'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
