b8ceb09c7f254524e8e965334ed059846b5526ae
gab-i-alves <gab.alvesnacimentO@gmail.com>
2024-12-01 12:55:05 -0300

    feat: Add article detail view for guest

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from .models import Article
 
@@ -11,8 +11,9 @@ def home(request):
     ).order_by('-date_published')
     return render(request, 'blog/home.html', {'articles': articles})
 
-def article():
-    ...
+def article_detail(request, id):
+    article = get_object_or_404(Article, id=id, date_published__lte=timezone.now())
+    return render(request, 'blog/article.html', {'article': article})
 
 # Admin Section
 def dashboard():
